9d7e08eeed8c87189e98a5fbf266112b352d8fed
Gustavo Moura <gustavopereira0@yahoo.com>
2023-12-07 03:53:21 -0300

    Add webserial and try to read PIR

--- include/config.h
+++ include/config.h
@@ -7,3 +7,5 @@
 const char OTA_HOSTNAME[] = "MotionWake";
 const char OTA_PASSWD[] = "123456";
 const uint16_t OTA_PORT = 8266;
+
+#define PIR_PIN D3
--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,70 @@
 #include "OTA.h"
 #include "WiFi.h"
 
- 
-void setup() {
+#include <NTPClient.h>
+
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <WebSerial.h>
+
+AsyncWebServer server(80);
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
+
+void recvMsg(uint8_t *data, size_t len)
+{
+  String d = "";
+  for (int i = 0; i < len; i++)
+  {
+    d += char(data[i]);
+  }
+  WebSerial.println(d);
+  if (d == "sleep")
+  {
+    WebSerial.println("Going to sleep now!!");
+    ESP.deepSleep(30e6);
+    delay(10);
+  }
+}
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PIR_PIN, INPUT);
+
   Serial.begin(115200);
-  Serial.println("\n\nIniciando...");
-  while (!connectWifi()) {
+  Serial.println("Conectando...");
+  while (!connectWifi())
+  {
     Serial.println("Conexao falhou! Reiniciando...");
     delay(5000);
     ESP.restart();
-  }   
- 
-  setUpOTA();
+  }
 
   Serial.println("Pronto");
   Serial.print("Endereco IP: ");
   Serial.println(getLocalIp());
+  setUpOTA();
 
-  pinMode(D4, OUTPUT);
-  pinMode(D3, INPUT);
+  WebSerial.begin(&server);
+  WebSerial.msgCallback(recvMsg);
+  server.begin();
+
+  timeClient.begin();
+  timeClient.setTimeOffset(-10800); // Offset is given in seconds, so GMT -3 is actually GMT -10800
 }
- 
-void loop() {
+
+void loop()
+{
   ArduinoOTA.handle();
-  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-  Serial.print("Movimento: ");
-  Serial.println(digitalRead(D3));
-  delay(200);
+
+  timeClient.update();
+
+  digitalWrite(LED_BUILTIN, !digitalRead(PIR_PIN));
+
+  String debug_msg = "Movimento: " + String(digitalRead(PIR_PIN)) + " | Horario: " + String(timeClient.getHours()) + ":" + String(timeClient.getMinutes()) + ":" + String(timeClient.getSeconds());
+  // WebSerial.println(debug_msg);
+  Serial.println(debug_msg);
+  delay(250);
 }
